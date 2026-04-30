4efabefe8a1de7ecbca743f4f6fbfd70f88ead35
Yusuf Ulgen <ysfulgen142@gmail.com>
2026-05-01 00:48:59 +0300

    sürüm yükseltildi.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yusufulgen.todolist"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 6
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
