c8642f0895ce549ee5eeb042aed7e2880cdd8d2d
Yusuf Ulgen <ysfulgen142@gmail.com>
2026-05-08 19:06:32 +0300

    sürüm yüskeltildi.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yusufulgen.todolist"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.4"
+        versionCode = 7
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
